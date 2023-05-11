2890e93b1985801724776bc1905b7f844eb1b218
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-05-10 22:02:53 -0300

    GET Game by Id

--- src/main/java/com/devrbernardes/dslist/services/GameService.java
+++ src/main/java/com/devrbernardes/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devrbernardes.dslist.dto.GameDTO;
 import com.devrbernardes.dslist.dto.GameMinDTO;
 import com.devrbernardes.dslist.entities.Game;
 import com.devrbernardes.dslist.repositories.GameRepository;
@@ -15,12 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
-	
-	
 	
 	
 	
