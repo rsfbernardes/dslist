628bb86e3fbc3ae691774f3baabcc2f06c8c5c63
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-05-10 21:05:45 -0300

    Database seed update

--- src/main/java/com/devrbernardes/dslist/entities/BelongingPK.java
+++ src/main/java/com/devrbernardes/dslist/entities/BelongingPK.java
@@ -14,7 +14,7 @@ public class BelongingPK {
 	private Game game;
 	
 	@ManyToOne
-	@JoinColumn(name = "game_list_id")
+	@JoinColumn(name = "list_id")
 	private GameList list;
 	
 	public BelongingPK() {
