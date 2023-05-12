6b6c7c2900c35ca48940a4602e71c1f2823d9051
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-05-12 16:18:11 -0300

    Deploy ready

--- src/main/java/com/devrbernardes/dslist/services/GameService.java
+++ src/main/java/com/devrbernardes/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devrbernardes.dslist.dto.GameDTO;
 import com.devrbernardes.dslist.dto.GameMinDTO;
 import com.devrbernardes.dslist.entities.Game;
+import com.devrbernardes.dslist.projections.GameMinProjection;
 import com.devrbernardes.dslist.repositories.GameRepository;
 
 @Service
@@ -29,6 +30,9 @@ public class GameService {
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
-	
-	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 }
